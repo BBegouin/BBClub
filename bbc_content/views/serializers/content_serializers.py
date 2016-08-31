fd1ce1bf7e952dcdffba9c134a4cfdca5d96d7c4
Bertrand <bertrand.begouin@gmail.com>
2016-08-31 21:32:32 +0200

    ===== Ajout des infos utilisateurs directement dans la description utilisateur =====

--- bbc_content/views/serializers/content_serializers.py
+++ bbc_content/views/serializers/content_serializers.py
@@ -1,14 +1,17 @@
 __author__ = 'Bertrand'
 from mezzanine.blog.models import BlogPost
 from rest_framework import serializers
+from bbc_user.views.serializers.user_serializer import UserSerializer
 
 class BlogPostDescriptionSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False,read_only=True);
+    
     class Meta:
         model = BlogPost
         exclude = ('content','in_sitemap','gen_description','site','short_url','allow_comments')
 
 class BlogPostDetailSerializer(serializers.ModelSerializer):
-    #user = serializers.ReadOnlyField(source='user.id')
+    user = UserSerializer(many=False,read_only=True);
 
     class Meta:
         model = BlogPost
