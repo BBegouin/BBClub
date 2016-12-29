66686d3f6e81310d0e584a57f68b808a7d84ce1c
Bertrand <bertrand.begouin@gmail.com>
2016-12-29 18:06:57 +0100

    ===== ajout du like sur les descriptions de post ====

--- bbc_content/views/serializers/content_serializers.py
+++ bbc_content/views/serializers/content_serializers.py
@@ -3,9 +3,16 @@ from mezzanine.blog.models import BlogPost
 from rest_framework import serializers
 from bbc_user.views.serializers.user_serializer import UserSerializer
 from django_comments.models import Comment
+from bbc_content.models.like import Like
+
 
 class BlogPostDescriptionSerializer(serializers.ModelSerializer):
-    user = UserSerializer(many=False,read_only=True);
+    user = UserSerializer(many=False,read_only=True)
+    likes = serializers.SerializerMethodField()
+
+    #on renvoi le nombre de like pour le post
+    def get_likes(self, obj):
+        return Like.objects.filter(post=obj).count();
 
     class Meta:
         model = BlogPost
@@ -17,8 +24,14 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
 
 class BlogPostDetailSerializer(serializers.ModelSerializer):
-    user = UserSerializer(many=False,read_only=True);
-    Comment = CommentSerializer(many=True,read_only=True);
+    user = UserSerializer(many=False,read_only=True)
+    Comment = CommentSerializer(many=True,read_only=True)
+    likes = serializers.SerializerMethodField()
+
+    #on renvoi le nombre de like pour le post
+    def get_likes(self, obj):
+        return Like.objects.filter(post=obj).count()
+
 
     class Meta:
         model = BlogPost
@@ -27,5 +40,10 @@ class BlogPostCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BlogPost
+
+class LikeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Like
 
 
